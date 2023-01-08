3d73775b78abbefe343cffaf2b5b068fb90e4d1b
David <enzobautista1000@gmail.com>
2023-01-07 22:58:40 -0300

    db ready

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -20,4 +20,6 @@ export class TasksComponent {
       this.tasks = tasks
     });
   }
+
+  
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { TASKS } from '../components/button/mock-tasks';
 import { Task } from '../components/button/Task';
@@ -7,10 +8,11 @@ import { Task } from '../components/button/Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTasks(): Task[]{
-    return TASKS;
+  getTasks(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
