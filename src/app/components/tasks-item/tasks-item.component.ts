165d48597a0fcb574792ff2302ea99bdeb854dc6
David <enzobautista1000@gmail.com>
2023-01-08 00:01:49 -0300

    save

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Task } from '../button/Task';
 import { TASKS } from '../button/mock-tasks';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -10,9 +10,10 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TasksItemComponent {
   @Input() task: Task = TASKS[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes;
 
-  onDelete(){
-    console.log("DELETE!")
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
   }
 }
