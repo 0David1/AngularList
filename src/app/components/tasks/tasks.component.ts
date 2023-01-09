28d5682eb11063abd84d5d7aa58e3c7f970e62de
David <enzobautista1000@gmail.com>
2023-01-09 11:57:02 -0300

    save

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -32,5 +32,10 @@ export class TasksComponent implements OnInit{
     this.taskService.updateTaskReminder(task).subscribe();
   }
 
-  
+  addTask(task: Task){
+    console.log(task);
+    this.taskService.addTask(task).subscribe((task)=>{
+      this.tasks.push(task);
+  })
   }
+}
