85e0dce799c205c0ffdfe2dc7a891c2964bbc0f3
David <enzobautista1000@gmail.com>
2023-01-10 22:23:29 -0300

    save

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   title: string = 'Task List';
-  showAddTask: boolean = true;
+  showAddTask: boolean = false;
   subscription?: Subscription;
 
   constructor(
